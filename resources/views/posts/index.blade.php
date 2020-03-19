63c62979349f3f789cf192247b1ba3b48a7c50ac
nousi <nousidayo@gmail.com>
2020-03-19 14:33:12 +0900

    web.phpにshowを追加

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/styles.css">
+  <title>myblog</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Blog Posts</h1>
+    <ul>
+      @forelse ($posts as $post)
+      <li><a href="">{{ $post->title }}</a></li>
+      @empty
+      <li>No posts yet</li>
+      @endforelse
+    </ul>
+  </div>
+  
+</body>
+</html>
