b935666cf9936d22c8b86070d9dadce7447f4e93
nousi <nousidayo@gmail.com>
2020-03-22 22:28:47 +0900

    showの部分テンプレート作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,7 +8,20 @@ use App\Post;
 class PostsController extends Controller
 {
     public function index() {
-        $posts = Post::all();
-        return view('posts.index', ['posts'=> $posts]);
+      // $posts = \App\Post::all();
+      // $posts = Post::all();
+      // $posts = Post::orderBy('created_at', 'desc')->get();
+      $posts = Post::latest()->get();
+      // $posts = [];
+      // dd($posts->toArray()); // dump die
+      // return view('posts.index', ['posts' => $posts]);
+      return view('posts.index')->with('posts', $posts);
+    }
+
+    // public function show($id) {
+    public function show(Post $post) {
+      // $post = Post::find($id);
+      // $post = Post::findOrFail($id);
+      return view('posts.show')->with('post', $post);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,7 +12,7 @@
     <h1>Blog Posts</h1>
     <ul>
       @forelse ($posts as $post)
-      <li><a href="">{{ $post->title }}</a></li>
+      <li><a href="{{ action('PostsController@show, $post->id'}}">{{ $post->title }}</a></li>
       @empty
       <li>No posts yet</li>
       @endforelse
